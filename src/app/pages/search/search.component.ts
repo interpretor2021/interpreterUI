9db65f44702ab921e1721f5abc1a29e3b9923ab1
Jeevan Rokade <interpretor2021@gmail.com>
2021-02-25 19:42:18 +0530

    commit file search functionality

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -8,17 +8,18 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  files:any=[]
-  fileURL=''
-  filecontent=''
-  searchTxt:string=''
-  fileType:string=''
+  files: any = []
+  fileURL = ''
+  filecontent = ''
+  searchTxt: string = ''
+  fileType: string = ''
+  selectedFile: any
 
-  types : any =[{value:"all", title:'ALL'},
-                        {value:"pdf", title:'PDF'},
-                        {value:"txt", title:'TXT'},
-                        {value:"jpg", title:'JPG'}]
-  
+  types: any = [{ value: "all", title: 'ALL' },
+  { value: "pdf", title: 'PDF' },
+  { value: "txt", title: 'TXT' },
+  { value: "jpg", title: 'JPG' }]
+
   modalRef!: BsModalRef;
   constructor(private searchService: SearchService, private modalService: BsModalService) { }
 
@@ -27,40 +28,41 @@ export class SearchComponent implements OnInit {
     this.getFile()
   }
 
-  openModal(template: TemplateRef<any>, file:any) {
+  openModal(template: TemplateRef<any>, file: any) {
     this.modalRef = this.modalService.show(template);
-    this.fileURL= file.filename
-    console.log('he---------', file,this.fileURL)
+    this.fileURL = file.filename
+    this.selectedFile = file
+    console.log('he---------', file, this.fileURL)
     //if(file.filetype !=)
 
-    this.searchService.getFilePreview(this.fileURL).subscribe((data:any) =>{
+    this.searchService.getFilePreview(this.fileURL).subscribe((data: any) => {
       this.filecontent = data.response;
       console.log('this:', this.files)
     })
 
   }
 
-  getFiles(){
-    this.searchService.getFiles().subscribe((data) =>{
-      this. files= data;
+  getFiles() {
+    this.searchService.getFiles().subscribe((data) => {
+      this.files = data;
       console.log('this:', this.files)
     })
   }
 
-  getFile(){
-    this.searchService.getFile().subscribe((data) =>{
+  getFile() {
+    this.searchService.getFile().subscribe((data) => {
       // this. files= data;
       console.log('this data:', data)
     })
   }
 
-  searchText(text:string, type:string){
-    this.searchService.searchtext(this.searchTxt, type).subscribe((data) =>{
-      this. files= data;
+  searchText(text: string, type: string) {
+    this.searchService.searchtext(this.searchTxt, type).subscribe((data) => {
+      this.files = data;
       console.log('this:', this.files)
     })
   }
 
-  
+
 
 }
